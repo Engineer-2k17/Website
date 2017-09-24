a98e9943f5231dfaec39adffea0eab1c40db665f
Vighnesh <vighneshvelayudhan@gmail.com>
2017-09-25 00:06:17 +0530

    menu, nav added

---
+++ js/loader.js
@@ -0,0 +1,6 @@
+$(document).ready(function(){
+
+	// loading the footer content
+	$('#footer').load('footer.html');
+	$("#nav").load('nav.html')
+})
--- js/main.js
+++ js/main.js
@@ -1,3 +1,27 @@
-$(function() {
-	$(function(){$('#footer').load('footer.html')});
+
+$(document).ready(function(){
+	init();
+	/* The button click and menu */
+	$(".ninja-btn").click(function() {
+            $(this).toggleClass("active");
+            if ($("menu").hasClass("active")) {
+                $("menu").animate({
+                    opacity: "0.0"
+                });
+            } else
+                $("menu").animate({
+                    opacity: "1.0"
+                });
+
+            $("menu").toggleClass("active");
+        });
 });
+
+function init(){
+	$(window).scroll(function(){
+		var percentage = $(window).scrollTop()/$(window).outerHeight();
+		var opa = Math.round(percentage*100)/100
+		$(".fade_in").css({"opacity" : opa});
+		$(".fade_out").css({"opacity": 1-opa})
+	})
+}
